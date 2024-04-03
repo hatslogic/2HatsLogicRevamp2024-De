e77130b35d58ea08bebcf5c2fe7375b5ceee7c09
Vijith <vijith@2hatslogic.com>
2024-04-03 18:40:26 +0530

    footer and responsive fixes

--- src/assets/js/app/base.js
+++ src/assets/js/app/base.js
@@ -1,5 +1,9 @@
+// Marquee effect
 const marqueeElement = document.querySelector('.marquee .marquee__content');
 if(marqueeElement != null){
     const parentElement = marqueeElement.parentElement;
     parentElement.insertBefore(marqueeElement.cloneNode(true), marqueeElement.nextSibling);
 }
+
+// Dynamic date for copyright
+document.getElementById("year").innerHTML = new Date().getFullYear();
