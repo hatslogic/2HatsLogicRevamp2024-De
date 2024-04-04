f42b31fa6c5f23e9fabe758e4b2ead2a95341f89
Vijith <vijith@2hatslogic.com>
2024-04-04 15:59:49 +0530

    refactor: optimization

--- src/assets/js/app/base.js
+++ src/assets/js/app/base.js
@@ -1,9 +1,9 @@
 // Marquee effect
-const marqueeElement = document.querySelector('.marquee .marquee__content');
-if (marqueeElement != null) {
-    const parentElement = marqueeElement.parentElement;
-    parentElement.insertBefore(marqueeElement.cloneNode(true), marqueeElement.nextSibling);
-}
+// const marqueeGroup = document.querySelector('.marquee .marquee__group');
+// if (marqueeGroup != null) {
+//     const parentElement = marqueeGroup.parentElement;
+//     parentElement.insertBefore(marqueeGroup.cloneNode(true), marqueeGroup.nextSibling);
+// }
 
 // Dynamic date for copyright
 document.getElementById("year").innerHTML = new Date().getFullYear();
