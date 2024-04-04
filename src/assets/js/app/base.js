06fc5fe1c60d6600bf4f834ad3e38edb689e09c8
Vijith <vijith@2hatslogic.com>
2024-04-04 12:08:29 +0530

    fix: scroll behaviour & flex adjustments

--- src/assets/js/app/base.js
+++ src/assets/js/app/base.js
@@ -1,6 +1,6 @@
 // Marquee effect
 const marqueeElement = document.querySelector('.marquee .marquee__content');
-if(marqueeElement != null){
+if (marqueeElement != null) {
     const parentElement = marqueeElement.parentElement;
     parentElement.insertBefore(marqueeElement.cloneNode(true), marqueeElement.nextSibling);
 }
