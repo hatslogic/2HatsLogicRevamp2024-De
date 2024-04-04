e45b3026fee57efc10112c721a49147a85a6529f
Vijith <vijith@2hatslogic.com>
2024-04-04 17:27:31 +0530

    fix: remove duplicate markup

--- src/assets/js/app/base.js
+++ src/assets/js/app/base.js
@@ -1,9 +1,9 @@
 // Marquee effect
-// const marqueeGroup = document.querySelector('.marquee .marquee__group');
-// if (marqueeGroup != null) {
-//     const parentElement = marqueeGroup.parentElement;
-//     parentElement.insertBefore(marqueeGroup.cloneNode(true), marqueeGroup.nextSibling);
-// }
+const marqueeGroup = document.querySelector('.marquee .marquee__group');
+if (marqueeGroup != null) {
+    const parentElement = marqueeGroup.parentElement;
+    parentElement.insertBefore(marqueeGroup.cloneNode(true), marqueeGroup.nextSibling);
+}
 
 // Dynamic date for copyright
 document.getElementById("year").innerHTML = new Date().getFullYear();
