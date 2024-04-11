e5737a3e84e5cfb79aad77a0aacd421cab474847
Vijith <vijith@2hatslogic.com>
2024-04-11 15:39:40 +0530

    feat: progress added

--- src/assets/js/app/base.js
+++ src/assets/js/app/base.js
@@ -7,3 +7,27 @@ if (marqueeGroup != null) {
 
 // Dynamic date for copyright
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+// Progress bar effect
+document.addEventListener("DOMContentLoaded", function() {
+    var progressBar = document.getElementById("progress");
+    var startTime = new Date().getTime();
+    
+    var interval = setInterval(function() {
+      var currentTime = new Date().getTime();
+      var elapsedTime = currentTime - startTime;
+      var progress = Math.min((elapsedTime / 800) * 100, 100); // Adjust the 3000 to your desired page load time in milliseconds
+      progressBar.style.width = progress + "%";
+      
+      if (progress >= 100) {
+        clearInterval(interval);
+      }
+    }, 10);
+  });
+
+// set timer of 1 sec after page load then add a class on body
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.querySelector('.hero mark').classList.add('loaded');
+    }, 800);
+})
