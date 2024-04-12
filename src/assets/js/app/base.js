617a4ac71c4f5415198933cc83549c4729ccbc76
Vijith <vijith@2hatslogic.com>
2024-04-12 18:15:11 +0530

    style: top menu rearrange

--- src/assets/js/app/base.js
+++ src/assets/js/app/base.js
@@ -24,33 +24,41 @@ document.addEventListener("DOMContentLoaded", function () {
 })
 
 // 
-
 let lastScrollTop = 0;
 let ticking = false;
+let isScrollingStopped = false;
 const stickyMenu = document.querySelector('.sticky-menu');
 
 function hideStickyMenu() {
-    stickyMenu.classList.remove('show');
+  stickyMenu.classList.remove('show');
 }
 
 function showStickyMenu() {
-    stickyMenu.classList.add('show');
+  stickyMenu.classList.add('show');
 }
 
 function updateScrollPosition() {
-    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-    if (currentScroll > lastScrollTop) {
-        hideStickyMenu();
-    } else {
-        showStickyMenu();
+  const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+  if (currentScroll > lastScrollTop) {
+    hideStickyMenu();
+  } else {
+    if (isScrollingStopped) {
+      showStickyMenu();
+      isScrollingStopped = false;
     }
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-    ticking = false;
+  }
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+  ticking = false;
 }
 
-window.addEventListener('scroll', function () {
-    if (!ticking) {
-        window.requestAnimationFrame(updateScrollPosition);
-        ticking = true;
-    }
+window.addEventListener('scroll', function() {
+  if (!ticking) {
+    window.requestAnimationFrame(updateScrollPosition);
+    ticking = true;
+  }
+  
+  clearTimeout(isScrollingStopped);
+  isScrollingStopped = setTimeout(function() {
+    isScrollingStopped = true;
+  }, 200); // Adjust this value as needed
 });
