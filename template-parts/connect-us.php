a5adeeac022d77ff3b4ccee68a7b68fccb86432f
wellwisherr <sania@2hatslogic.com>
2024-05-20 19:55:15 +0530

    contact page integration

---
+++ template-parts/connect-us.php
@@ -0,0 +1,104 @@
+<?php
+$image_1 = get_field('image_1');
+$image_2 = get_field('image_2');
+$name_1 =  get_field('contact_one_name');
+$name_2 = get_field('contact_two_name');
+$primary_email = get_field('primary_email');
+$desig1 = get_field('contact_one_designation');
+$desig2 = get_field('contact_two_designation');
+$loc1 = get_field('contact_one_location');
+$loc2 = get_field('contact_two_location');
+$call1 = get_field('call_one_button');
+$call2 = get_field('call_two_button');
+
+
+?>
+<section class="connect-us py-60 xs:pt-0">
+                 <div class="container">
+                     <div class="title">
+                          <h2>Connect us</h2>
+ 
+                     </div>
+                     <div class="content gap-0 xl:mt-40">
+                         <div class="flex xl:wrap justify-between align-center xl:align-start gap-30 xl:gap-0">
+                             <div class="col w-100 xl:w-50 xs:w-100 pr-40 xl:pr-30 xs:pr-0 b-0 br-1 xs:br-0 solid bc-hash">
+                                 <div class="avatar-wrap flex">
+                                     <div class="img-wrap w-px-80 h-px-80 max-w-px-80 min-w-px-80 xs:w-px-60 xs:h-px-60 xs:max-w-px-60 xs:min-w-px-60">
+                                         <img src="<?php echo $image_1['url'] ?>" alt="<?php echo $name_1 ?>" width="80" height="80">
+                                     </div>
+                                     <div class="author ml-20">
+                                         <div class="author-name font-bold fs-20"><?php echo $name_1 ?></div> <span class="designation font-light fs-15 lh-1-25 mt-5 inline-block"><?php echo $desig1; ?>,<br> <small><?php echo $loc1; ?>&period;</small></span>
+ 
+                                     </div>
+                                 </div> 
+                                 <?php  if($call1): ?>
+                                 <a href="tel:<?php echo $call1 ?>" class="bg-transparent px-0 mt-20 xs:mt-10 xs:ml-80 fs-18 xs:fs-16 c-secondary hover:c-primary b-0 font-bold py-10 bc-transparent inline-flex align-center gap-15 xs:gap-10">
+                         <i class="icomoon icon-calendar_month fs-24"></i> Schedule a call
+                     </a>
+ <?php endif; ?>
+                             </div>
+                             <div class="col w-100 xl:w-50 xs:w-100 pl-40 xl:pl-30 xs:pl-0 md:mt-30 xs:mt-20 pr-40 xl:pr-20 xs:pr-0 xs:pt-20 b-0 br-1 xs:br-0 md:br-0 solid bc-hash">
+                                 <div class="avatar-wrap flex">
+                                     <div class="img-wrap w-px-80 h-px-80 max-w-px-80 min-w-px-80 xs:w-px-60 xs:h-px-60 xs:max-w-px-60 xs:min-w-px-60">
+                                         <img src="<?php echo $image_2['url'] ?>" alt="<?php echo $name_2 ?>" width="80" height="80">
+                                     </div>
+                                     <div class="author ml-20">
+                                         <div class="author-name font-bold fs-20"><?php echo $name_2 ?></div> <span class="designation font-light fs-15 lh-1-25 mt-5 inline-block"><?php echo $desig2; ?>,<br> <small><?php echo $loc2; ?>&period;</small></span>
+ 
+                                     </div>
+                                 </div>
+                                 <?php if($call2): ?>
+                                 <a href="tel:<?php echo $call2 ?>" class="bg-transparent px-0 mt-20 xs:mt-10 xs:ml-80 fs-18 xs:fs-16 c-secondary hover:c-primary b-0 font-bold py-10 bc-transparent inline-flex align-center gap-15 xs:gap-10">
+                         <i class="icomoon icon-calendar_month fs-24"></i> Schedule a call
+                     </a>
+
+                                 <?php endif; ?>
+                             </div>
+                             <div class="col w-100 b-0 xl:bt-1 xl:mt-40 xl:pt-40 solid bc-hash pl-60 xl:pl-0 md:mt-40">
+                                 <div class="card flex align-center">
+                                     <div class="wrap-icon min-w-px-30 fs-28"> <i class="icomoon icon-mail"></i>
+ 
+                                     </div>
+                                     <div class="info ml-20">
+                                          <h3 class="h5 transition font-regular opacity-50 mb-5">Email</h3>
+ 
+                                         <p class="mt-0 mb-0 fs-16 font-regular">E: <a class="c-secondary hover:c-primary hover:text-decoration-underline" href="mailto:<?php echo $primary_email; ?>"><?php echo $primary_email; ?></a>
+ 
+                                         </p>
+                                     </div>
+                                 </div>
+                                 <div class="card flex align-center mt-30">
+                                     <div class="wrap-icon min-w-px-30 fs-28"> <i class="icomoon icon-headphones"></i>
+ 
+                                     </div>
+                                     <div class="info ml-20">
+                                          <h3 class="h5 transition font-regular opacity-50 mb-5">Career</h3>
+ 
+                                         <p class="mt-0 mb-0 fs-16 font-regular">E: <a class="c-secondary hover:c-primary hover:text-decoration-underline" href="<?php the_field('contact_mail');?>"><?php the_field('contact_mail'); ?></a>
+ 
+                                         </p>
+                                         <p class="mt-0 mb-0 fs-16 font-regular">P: <a class="c-secondary hover:c-primary hover:text-decoration-underline" href="tel:<?php the_field('contact_phone');?>"><?php the_field('contact_phone');?></a>
+ 
+                                         </p>
+                                     </div>
+                                 </div>
+                                 <div class="card flex align-center mt-30">
+                                     <div class="wrap-icon min-w-px-30 fs-28"> <i class="icomoon icon-call"></i>
+ 
+                                     </div>
+                                     <div class="info ml-20">
+                                          <h3 class="h5 transition font-regular opacity-50 mb-5">Office</h3>
+                                <?php if(have_rows('office_phone')): 
+                                    while(have_rows('office_phone')): the_row(); ?>
+                                         <p class="mt-0 mb-0 fs-16 font-regular"><span class="font-bold"><?php the_sub_field('country') ?></span> : <a class="c-secondary hover:c-primary hover:text-decoration-underline" href="tel:+<?php the_sub_field('phone');?>">+<?php the_sub_field('phone');?></a>
+ 
+                                         </p>
+                                <?php endwhile; 
+                            endif; ?>
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </section>
---
+++ template-parts/contact-hero.php
@@ -0,0 +1,83 @@
+<?php 
+$get_quote_form = get_field('get_quote_contact_form'); 
+ $partnership_form = get_field('get_parrnership_form'); 
+$contact_form_image_1 = get_field('contact_form_image_1');
+$contact_form_image_2 = get_field('contact_form_image_2');
+$form_1_title = get_field('form_1_title');
+$form_2_title = get_field('form_2_title');
+$form_1_description = get_field('form_1_description');
+$form_2_description = get_field('form_2_description');
+?>
+<?php if($get_quote_form || $partnership_form): ?>
+<section class="hero py-60">
+    <div class="container">
+        <div class="contact-wrap transition show" id="get-a-quote">
+            <div class="flex align-start justify-between md:wrap">
+                <div class="col w-50 mr-50 xl:mr-30 md:mr-0 md:w-100 md:mt-40">
+                    <picture>
+                        <source srcset="<?php echo esc_url($contact_form_image_1['sizes']['img_606x749']); ?>" type="image/webp">
+                        <source srcset="<?php echo esc_url($contact_form_image_1['sizes']['img_606x749']); ?>" type="image/jpg">
+                        <img src="<?php echo esc_url($contact_form_image_1['sizes']['img_606x749']); ?>" loading="lazy" alt="contact-1" width="606px"
+                            height="749px" class="transition">
+                    </picture>
+                </div>
+                <div class="col w-50 ml-50 xl:ml-30 md:ml-0 md:mt-40 md:w-100">
+                    <div class="form-wrap">
+                        <div class="title">
+                            <h2> <?php echo $form_1_title; ?></h2>
+
+                            <p class="opacity-70"><?php echo $form_1_description; ?></p>
+                        </div>
+                        <div class="content">
+                            <div class="switch-form-actions mt-40 xl:mt-30">
+                                <button data-target="get-a-quote"
+                                    class="fs-18 bg-transparent px-0 active c-secondary opacity-100 font-bold b-0 bb-2 py-10 bc-secondary c-secondary solid">Get
+                                    a Quote</button>
+                                <button data-target="partnership"
+                                    class="fs-18 bg-transparent px-0 c-secondary opacity-50 b-0 bb-2 font-bold py-10 bc-transparent solid ml-40 xl:ml-20">Partnership</button>
+                            </div>
+                            <?php
+                            if ($get_quote_form): ?>
+                                <?php echo do_shortcode('[contact-form-7 id="' . $get_quote_form->ID . '"]'); ?>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="contact-wrap transition" id="partnership">
+            <div class="flex align-start justify-between md:wrap" id="partnership">
+                <div class="col w-50 mr-50 xl:mr-30 md:mr-0 md:w-100 md:mt-40">
+                    <picture>
+                    <source srcset="<?php echo esc_url($contact_form_image_2['sizes']['img_606x749']); ?>" type="image/webp">
+                        <source srcset="<?php echo esc_url($contact_form_image_2['sizes']['img_606x749']); ?>" type="image/jpg">
+                        <img src="<?php echo esc_url($contact_form_image_2['sizes']['img_606x749']); ?>" loading="lazy" alt="contact-2" width="648px"
+                            height="445px" class="transition">
+                    </picture>
+                </div>
+                <div class="col w-50 ml-50 xl:ml-30 md:ml-0 md:mt-40 md:w-100">
+                    <div class="form-wrap">
+                        <div class="title">
+                            <h2><?php echo $form_2_title; ?></h2>
+
+                            <p class="opacity-70"><?php echo $form_2_description; ?></p>
+                        </div>
+                        <div class="content">
+                            <div class="switch-form-actions mt-40 xl:mt-30">
+                                <button data-target="get-a-quote"
+                                    class="fs-18 bg-transparent px-0 c-secondary opacity-50 b-0 bb-2 font-bold py-10 bc-transparent solid">Get
+                                    a Quote</button>
+                                <button data-target="partnership"
+                                    class="fs-18 bg-transparent px-0 active c-secondary opacity-100 font-bold b-0 bb-2 py-10 bc-secondary c-secondary solid ml-40 xl:ml-20">Partnership</button>
+                            </div>
+                            <?php if($partnership_form): ?>
+                                <?php echo do_shortcode('[contact-form-7 id="' . $partnership_form->ID . '"]'); ?>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php endif; ?>
