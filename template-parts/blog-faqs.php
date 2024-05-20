d46ed8879c8e0f60f8dfde702cca680a50113903
wellwisherr <sania@2hatslogic.com>
2024-05-20 13:11:57 +0530

    faq - blog deatils

--- template-parts/blog-faqs.php
+++ template-parts/blog-faqs.php
@@ -1,35 +1,30 @@
-<?php
-if (get_field('enable_faq')): ?>
+<?php if (get_field('enable_faq')): ?>
 <section class="blogs_faq">
-    <div class="content">
-        <div class="title">
-            <h3><?php the_field('faq_title'); ?></h3>
-        </div>
-        <?php if (have_rows('faq_group')): ?>
-        <div class="content mt-20 gap-40 xs:mt-10">
-            <div class="acc">
-            <?php while (have_rows('faq_group')):
-                            the_row(); ?>
-                <div class="acc-item py-20 b-0 bb-1 bc-hash solid">
-                    <div class="acc-toggle flex justify-between relative">
-                        <h4 class="max-w-80"><?php the_sub_field('faq_question');?></h4 class="max-w-80">
-                        <div class="wrap-icon absolute right-0 flex fs-30 xs:fs-20">
-                            <div class="icomoon icon-expand_circle_down add"></div>
-                            <div class="icomoon icon-expand_circle_up minus"></div>
-                        </div>
-                    </div>
-                    <div class="acc-content">
-                        <div class="inner w-100">
-                            <p><?php the_sub_field('faq_answer');?></p>
-                        </div>
+    <div class="title">
+        <h3><?php the_field('faq_title'); ?></h3>
+    </div>
+    <?php if (have_rows('faq_group')): ?>
+    <div class="content mt-20 gap-40 xs:mt-10">
+        <div class="acc">
+        <?php while (have_rows('faq_group')):
+                        the_row(); ?>
+            <div class="acc-item py-20 b-0 bb-1 bc-hash solid">
+                <div class="acc-toggle flex justify-between relative">
+                    <h4 class="max-w-80"><?php the_sub_field('faq_question');?></h4 class="max-w-80">
+                    <div class="wrap-icon absolute right-0 flex fs-30 xs:fs-20">
+                        <div class="icomoon icon-expand_circle_down add"></div>
+                        <div class="icomoon icon-expand_circle_up minus"></div>
                     </div>
                 </div>
-                <?php endwhile; ?>
+                <div class="acc-content">
+                    <div class="inner w-100">
+                        <p><?php the_sub_field('faq_answer');?></p>
+                    </div>
+                </div>
             </div>
+            <?php endwhile; ?>
         </div>
-                    <?php
-            endif; ?>
     </div>
+    <?php endif; ?>
 </section>
-  
 <?php endif; ?>
