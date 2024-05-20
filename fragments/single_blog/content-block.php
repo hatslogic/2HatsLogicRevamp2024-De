3cd40a3415d5d000e122d9b9a7b130c931871872
Vijith <vijith@2hatslogic.com>
2024-05-20 20:02:21 +0530

    cleanup - blog

--- fragments/single_blog/content-block.php
+++ fragments/single_blog/content-block.php
@@ -1,6 +1,8 @@
 <?php extract($section); ?>
 
-<div name="section-<?php echo $identifier; ?>" class="info mb-40 md:mb-20">
-            <h3><?php echo $heading; ?></h3>
-            <p><?php echo $content; ?> </p>
+<?php if ($heading || $content): ?>
+<div name="section-<?php echo ($identifier) ? $identifier : '0'; ?>" class="info mb-40">
+    <h3><?php echo $heading; ?></h3>
+    <p><?php echo $content; ?> </p>
 </div>
+<?php endif; ?>
--- src/assets/js/app/contact.js
+++ src/assets/js/app/contact.js
@@ -35,15 +35,17 @@ function makeRequest(method, url, callback) {
 }
 
 // Get user's IP address
-makeRequest('GET', 'http://ip-api.com/json/', function(response) {
-    var data = JSON.parse(response);
-    var countryCode = data.countryCode; // Extract the country code from the response
+if(input) {
+    makeRequest('GET', 'http://ip-api.com/json/', function(response) {
+        var data = JSON.parse(response);
+        var countryCode = data.countryCode; // Extract the country code from the response
 
-    var iti = window.intlTelInput(input, {
-        utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
-        separateDialCode: true,
-        autoHideDialCode: false,
-        preferredCountries: ['us', 'gb', 'in'],
-        initialCountry: countryCode.toLowerCase(), // Use the country code obtained from IP
-    });
-});
+        var iti = window.intlTelInput(input, {
+            utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+            separateDialCode: true,
+            autoHideDialCode: false,
+            preferredCountries: ['us', 'gb', 'in'],
+            initialCountry: countryCode.toLowerCase(), // Use the country code obtained from IP
+        });
+    });   
+}
