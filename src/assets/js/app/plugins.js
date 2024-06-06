cb06d6c7da700dc1eabb4a84b631487217201dce
Vijith <vijith@2hatslogic.com>
2024-06-06 12:02:50 +0530

    adding autoplay for plugins slider

--- src/assets/js/app/plugins.js
+++ src/assets/js/app/plugins.js
@@ -1,7 +1,7 @@
 const plugins = document.querySelector("#plugins");
 
 if(plugins !== null){
-    var testimonialSlider = new HatsSlider("#plugins", {
+    var pluginsSlider = new HatsSlider("#plugins", {
             loop: true,
             auto: true,
             mode: "snap",
@@ -17,6 +17,36 @@ if(plugins !== null){
             slides: {
                 perView: "auto"
             },
-        }
+        },
+        [
+            (pluginsSlider) => {
+              let timeout
+              let mouseOver = false
+              function clearNextTimeout() {
+                clearTimeout(timeout)
+              }
+              function nextTimeout() {
+                clearTimeout(timeout)
+                if (mouseOver) return
+                timeout = setTimeout(() => {
+                    pluginsSlider.next()
+                }, 2000)
+              }
+              pluginsSlider.on("created", () => {
+                pluginsSlider.container.addEventListener("mouseover", () => {
+                  mouseOver = true
+                  clearNextTimeout()
+                })
+                pluginsSlider.container.addEventListener("mouseout", () => {
+                  mouseOver = false
+                  nextTimeout()
+                })
+                nextTimeout()
+              })
+              pluginsSlider.on("dragStarted", clearNextTimeout)
+              pluginsSlider.on("animationEnded", nextTimeout)
+              pluginsSlider.on("updated", nextTimeout)
+            },
+          ]
     )
 }
