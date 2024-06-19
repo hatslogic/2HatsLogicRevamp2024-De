294f400960e628e75e30bc8db29ac933c5c17ca0
Radhu2hats <radhu@2hatslogic>
2024-06-19 11:32:58 +0530

    - Career page initial commit

---
+++ templates/template-career.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: Template Career
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package 2HatsLogic
+ */
+
+get_header();
+?>
+
+<main class="page-wrap inline-block w-100">
+    <?php app_render_page_career(); ?>
+</main>
+
+<?php
+if ( get_field( 'show_callout' ) ) {
+    app_render_fragment( 'global-callout' );
+}
+
+get_template_part( 'template-parts/free-consultation');
+?>
+
+<?php get_footer(); ?>
