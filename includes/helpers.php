2e4b8d45a6b83526cb43bfbdd0b0adb0421f209f
Jipto Saji <jipto@2hatslogic.com>
2024-06-20 16:45:42 +0530

    Help desk single page integration

--- includes/helpers.php
+++ includes/helpers.php
@@ -268,3 +268,15 @@ function app_render_page_agency() {
 		app_render_fragment( 'agency' . DIRECTORY_SEPARATOR . $section_slug, compact( 'section_index', 'section_slug', 'section' ) );
 	}
 }
+
+function app_render_page_single_help_desk() {
+	if ( ! $sections = get_field( 'single_help_desk' ) ) {
+		return;
+	}
+
+	foreach ( $sections as $section_index => $section ) {
+		$section_slug = str_replace( '_', '-', $section['acf_fc_layout'] );
+
+		app_render_fragment( 'single_help_desk' . DIRECTORY_SEPARATOR . $section_slug, compact( 'section_index', 'section_slug', 'section' ) );
+	}
+}
