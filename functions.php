9c2fc5e1c1df0e7c1d58ca8802bffe74e7265c89
Vijith <vijith@2hatslogic.com>
2024-06-26 19:20:29 +0530

    version changes for cache referesh

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ define('APP_THEME_DIR', dirname(__FILE__) . DIRECTORY_SEPARATOR);
 
 if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define('_S_VERSION', '1.0.0');
+	define('_S_VERSION', '1.1.0');
 }
 
 
