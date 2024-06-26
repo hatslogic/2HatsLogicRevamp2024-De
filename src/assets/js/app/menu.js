9805342d4c3d0816ba478e567091abfa4f76cc40
Vijith <vijith@2hatslogic.com>
2024-06-26 19:03:27 +0530

    mobile menu performance & style enhacements

--- src/assets/js/app/menu.js
+++ src/assets/js/app/menu.js
@@ -1,25 +1,30 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.top-menu');
-const hasChild = document.querySelectorAll('.main-menu > li.has-child > a');
+const hasChild = document.querySelectorAll('.main-menu > li.has-child');
 
-if(menuBtn !== null) {
+if (menuBtn !== null) {
     menuBtn.addEventListener('click', (e) => {
         e.preventDefault();
         menuBtn.classList.toggle('active');
         menu.classList.toggle('active');
-    });   
+    });
 }
 
-if(hasChild !== null && window.matchMedia('(max-width: 1024px)').matches) {
+if (hasChild !== null && window.matchMedia('(max-width: 1024px)').matches) {
     for (let i = 0; i < hasChild.length; i++) {
         hasChild[i].addEventListener('click', (e) => {
             e.preventDefault();
+            let parentLi = e.target.closest('li'); // Use closest to ensure parentLi is the <li> element
+
+            // Close all other submenus
             for (let j = 0; j < hasChild.length; j++) {
-                if (j !== i) {
-                    hasChild[j].parentNode.classList.remove('active');
+                if (hasChild[j] !== hasChild[i]) { // Check to avoid toggling the same submenu twice
+                    hasChild[j].classList.remove('active');
                 }
             }
-            e.target.parentNode.classList.toggle('active');
+
+            // Toggle the clicked submenu
+            parentLi.classList.toggle('active');
         });
     }
 }
@@ -27,4 +32,8 @@ if(hasChild !== null && window.matchMedia('(max-width: 1024px)').matches) {
 function closeMenu() {
     menuBtn.classList.remove('active');
     menu.classList.remove('active');
+    // Close all submenus when the main menu is closed
+    for (let i = 0; i < hasChild.length; i++) {
+        hasChild[i].classList.remove('active');
+    }
 }
