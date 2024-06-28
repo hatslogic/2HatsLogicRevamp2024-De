0e17400307a7b40589c936e8c22d944f5fd2d9a6
Vijith <vijith@2hatslogic.com>
2024-06-28 15:30:37 +0530

    moved the titles to bottom after career list & apply now position  corrected

--- fragments/career/career-list.php
+++ fragments/career/career-list.php
@@ -6,12 +6,6 @@
             <?php if (!empty($header['main_title'])): ?>
             <h2><?= $header['main_title']; ?></h2>
             <?php endif; ?>
-            <?php if (!empty($header['sub_title'])): ?>
-            <h3 class="h4 mt-40"><?= $header['sub_title']; ?></h3>
-            <?php endif; ?>
-            <?php if (!empty($header['content'])): ?>
-            <p><?= $header['content']; ?></p>
-            <?php endif; ?>
         </div>
 
         <?php if (!empty($openings)): ?>
@@ -19,11 +13,11 @@
             <div class="list-wrap b-0 bb-1 bc-hash solid">
                 <?php foreach ($openings as $opening): ?>
                 <?php if (empty($opening['job_title'])) continue; ?>
-                <div class="item flex md:wrap align-center justify-between b-0 bt-1 bc-hash solid py-30">
+                <div class="item flex md:wrap align-center justify-between b-0 bt-1 bc-hash solid py-30 relative">
                     <h4 class="w-50 md:w-100 md:mb-15"><?= $opening['job_title']; ?></h4>
-                    <div class="positions w-25 md:w-50"><?= $opening['no_of_positions']; ?></div>
-                    <div class="experience w-25 md:w-50"><?= $opening['experience']; ?></div>
-                    <div class="apply-now w-25 md:w-100 md:mt-20 flex align-center justify-end md:justify-start">
+                    <div class="positions w-50 md:w-100"><?= $opening['no_of_positions']; ?></div>
+                    <div class="experience w-25 md:w-100 mt-10"><?= $opening['experience']; ?></div>
+                    <div class="apply-now w-25 md:w-100 flex align-center justify-end md:absolute md:right-0 md:bottom-28">
                         <a href="https://mail.google.com/mail/?view=cm&fs=1&to=hr@2hatslogic.com" target="_blank" class="flex align-center gap-10">Apply Now
                             <i class="icon-arrow_circle_right fs-22"></i>
                         </a>
@@ -31,10 +25,17 @@
                 </div>
                 <?php endforeach; ?>
             </div>
+
+            <?php if (!empty($header['sub_title'])): ?>
+            <h3 class="h4 mt-60"><?= $header['sub_title']; ?></h3>
+            <?php endif; ?>
+            <?php if (!empty($header['content'])): ?>
+            <p><?= $header['content']; ?></p>
+            <?php endif; ?>
+
             <?php if (!empty($button)): ?>
-            <div class="btn-group mt-60 center">
-                <a href="<?= htmlspecialchars($button['url']); ?>"
-                    class="btn btn-secondary"><?= htmlspecialchars($button['title']); ?></a>
+            <div class="btn-group mt-60">
+                <a href="<?= htmlspecialchars($button['url']); ?>" class="btn btn-secondary"><?= htmlspecialchars($button['title']); ?></a>
             </div>
             <?php endif; ?>
         </div>
