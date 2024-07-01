57e3d86ec32593985426c3540bc4424c4c31cbd5
Radhu2hats <radhu@2hatslogic>
2024-07-01 11:02:22 +0530

    Added the list items to the sections in case study

--- fragments/case_studies/problem.php
+++ fragments/case_studies/problem.php
@@ -9,6 +9,15 @@
         <?php endif; ?>
         <div class="content">
             <p><?php echo $content; ?></p>
+            <?php if ($list): ?>
+                <ul class="no-bullets split-2 mt-30 xs:split-1 xs:fs-14">
+                    <?php foreach ($list as $item): ?>
+                        <li class="mb-10 flex"><i class="icomoon icon-check_circle fs-16 c-primary mt-4"></i> 
+                        <span class="ml-8"><?php echo $item['list_item']; ?></span>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
             <?php if ($images['image_1'] || $images['image_2']): ?>
             <div class="grid grid-2 gap-20 mt-60 xs:flex xs:wrap">
                 <?php if ($images['image_1']): ?>
