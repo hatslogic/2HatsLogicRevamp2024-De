3067b21b76dfe09323f4b22b4371e5f302e7b448
Radhu2hats <radhu@2hatslogic>
2024-07-12 09:37:28 +0530

    - Image reszing : Implementing the new function in about and team page

--- fragments/about/hero-section.php
+++ fragments/about/hero-section.php
@@ -4,27 +4,30 @@
         <div class="flex align-center justify-between md:wrap">
             <div class="col w-50 md:w-100">
                 <div class="headline">
-                    <?php if ($headline['title']): ?>
-                        <h1 class="h3"><?php echo $headline['subtitle'] ?></h1>
-                        <h2 class="h1-sml"><?php echo $headline['title'] ?></h2>
-                        <p class="mt-30"><?php echo $content ?></p>
-                    <?php endif; ?>
-                    <?php if ($button): ?>
+                    <?php if ($headline['title']) { ?>
+                        <h1 class="h3"><?php echo $headline['subtitle']; ?></h1>
+                        <h2 class="h1-sml"><?php echo $headline['title']; ?></h2>
+                        <p class="mt-30"><?php echo $content; ?></p>
+                    <?php } ?>
+                    <?php if ($button) { ?>
                         <div class="btn-group mt-40">
                             <a href="<?php echo $button['url']; ?>" class="btn btn-primary"><?php echo $button['title']; ?></a>
                         </div>
-                    <?php endif; ?>
+                    <?php } ?>
                 </div>
             </div>
-            <?php if ($image): ?>
+            <?php if ($image) { ?>
                 <div class="col w-40 md:w-100 md:mt-40">
-                <?php $cropOptions = [
-                        "fallbackimage-size" => [648,445],
-                        'fallbackimage-class'=> 'transition'
-                    ];?>
-                    <?php display_responsive_image($image['ID'],$cropOptions) ?>
+                <?php
+                    $cropOptions = [
+                        '(max-width: 768px)' => [335, 232],
+                        '(min-width: 769px)' => [615, 420],
+                    ];
+                $attributes = ['class' => 'transition', 'loading' => 'eager', 'fetchPriority' => 'high'];
+                ?>
+                    <?php echo hatslogic_get_attachment_picture($image['ID'], $cropOptions, $attributes); ?>
                 </div>
-            <?php endif; ?>
+            <?php } ?>
         </div>
     </div>
 </section>
--- fragments/team/team-members.php
+++ fragments/team/team-members.php
@@ -3,58 +3,63 @@
 <section class="meet-our-team overflow-hidden">
     <div class="container">
 
-        <?php if($title || $content): ?>
+        <?php if ($title || $content) { ?>
         <div class="title">
-            <?php if($title): ?>
+            <?php if ($title) { ?>
                 <h2> <?php echo $title; ?> </h2>
-            <?php endif; ?>
-            <?php if($content): ?>
+            <?php } ?>
+            <?php if ($content) { ?>
                 <p> <?php echo $content; ?> </p>
-            <?php endif; ?>
+            <?php } ?>
         </div>
-        <?php endif; ?>
+        <?php } ?>
         
-        <?php if ($select_members): ?>
+        <?php if ($select_members) { ?>
         <div class="content relative mt-60 xs:mt-30">
             <div class="grid grid-6 xl:grid-4 md:grid-3 xs:grid-2 cg-10 rg-40" id="members">
-                <?php 
-                foreach ($select_members as $key => $item):
-                if($key > 35) break;
-                setup_postdata($item);
-                $formatted_key = sprintf("%02d", $key + 1);
-                $name = get_field('name', $item->ID);
-                $designation = get_field('designation', $item->ID);
-                $image = get_the_post_thumbnail_url($item->ID, 'img_250x330');
-                $image_id = get_post_thumbnail_id($item->ID);
-                ?>
+                <?php
+                foreach ($select_members as $key => $item) {
+                    if ($key > 35) {
+                        break;
+                    }
+                    setup_postdata($item);
+                    $formatted_key = sprintf('%02d', $key + 1);
+                    $name = get_field('name', $item->ID);
+                    $designation = get_field('designation', $item->ID);
+                    $image = get_the_post_thumbnail_url($item->ID, 'img_250x330');
+                    $image_id = get_post_thumbnail_id($item->ID);
+                    ?>
                 <div class="item">
                     <div class="card">
                         <div class="img bg-secondary">
-                            <?php $cropOptions = [
-                                    "fallbackimage-size" => [250,330],
-                                    "fallbackimage-class" => "transition",
-                                    'aspect-ratio' => [185,243]
-                                    ];?>
-                            <?php display_responsive_image($image_id,$cropOptions) ?>
+                            
+                            <?php
+                                $cropOptions = [
+                                    '(max-width: 768px)' => [163, 213],
+                                    '(min-width: 769px)' => [246, 323],
+                                ];
+                    $attributes = ['class' => 'transition', 'loading' => 'lazy'];
+                    ?>
+                            <?php echo hatslogic_get_attachment_picture($image_id, $cropOptions, $attributes); ?>
                         </div>
                         <div class="info mt-10">
-                            <?php if($name): ?>
+                            <?php if ($name) { ?>
                                 <h3 class="h5 font-bold"><?php echo $name; ?></h3>
-                            <?php endif; ?>
-                            <?php if($designation): ?>
+                            <?php } ?>
+                            <?php if ($designation) { ?>
                                 <div class="font-light fs-14 mt-3"><?php echo $designation; ?></div>
-                            <?php endif; ?>
+                            <?php } ?>
                         </div>
                     </div>
                 </div>
-                <?php 
-                endforeach;
-                wp_reset_postdata();
-                ?>
+                <?php
+                }
+            wp_reset_postdata();
+            ?>
             </div>
             <div class="gradient-end h-px-300 w-100 block absolute bottom-0"></div>
         </div>
-        <?php endif; ?>
+        <?php } ?>
 
         <div class="btn-group center mt-60"> 
             <a href="#" class="btn btn-secondary" id="view-all-members">View All</a>
