80b4ae114c6613e91c1462a356745e558f6f5fb2
Radhu2hats <radhu@2hatslogic>
2024-07-15 09:20:21 +0530

    - Replacing the picture rework

--- fragments/sections/about.php
+++ fragments/sections/about.php
@@ -2,29 +2,32 @@
 <section class="about">
     <div class="container">
         <div class="content flex sm:wrap align-center">
-        <?php if($image): ?>
+        <?php if ($image) { ?>
             <div class="col w-100">
-                <?php $cropOptions = [
-                    "fallbackimage-size" => [494,328],
-                    "fallbackimage-class"=>"h-auto w-100",
-                    'aspect-ratio' => [694,567]
-                ];?>
-                <?php display_responsive_image($image['ID'],$cropOptions) ?>
+                
+                <?php
+                        $cropOptions = [
+                            '(max-width: 768px)' => [390, 260],
+                            '(min-width: 769px)' => [696, 462],
+                        ];
+            $attributes = ['class' => 'h-auto w-100', 'loading' => 'lazy'];
+            ?>
+                <?php echo hatslogic_get_attachment_picture($image['ID'], $cropOptions, $attributes); ?>
             </div>
-            <?php endif; ?>
+            <?php } ?>
             <div class="col w-100 ml-120 xxl:ml-80 xl:ml-60 sm:ml-0 sm:mt-40">
-            <?php if($headline['title']): ?>
+            <?php if ($headline['title']) { ?>
                 <div class="title">
                     <span class="headline c-primary font-bold"><?php echo $headline['sub_title']; ?></span>
                     <h2><?php echo $headline['title']; ?></h2>
                 </div>  
-                <?php endif; ?>
+                <?php } ?>
                 <div class="content">
                     <?php echo apply_filters('the_content', $content); ?>
                
-                    <?php if($link): ?>
+                    <?php if ($link) { ?>
                     <a href="<?php echo $link['url']; ?>" aria-label="<?php echo $link['title']; ?>" class="btn btn-secondary-outline"><?php echo $link['title']; ?></a></a>
-                      <?php endif; ?>
+                      <?php } ?>
                 </div>
             </div>
         </div>
--- template-parts/newsletter-form.php
+++ template-parts/newsletter-form.php
@@ -5,31 +5,33 @@ $form_selector = get_field('form_selector', 'option');
 ?>
 
 <section id="get-started" class="get-started bg-dark-primary c-white relative" id="get-started">
-    <?php if ($newsletter_image && $newsletter_image_mobile): ?>
-        <?php $cropOptions = [
-        "fallbackimage-size" => [800,540],
-        "fallbackimage-class" => "h-100 cover",
-        "picturetag-class" => "h-100 w-50 absolute top-0 sm:w-100 sm:relative"
-        ];
-        if($newsletter_image_mobile){
-
-            $cropOptions["mobile-settings"] = [
-                "image" => $newsletter_image_mobile['ID']
-            ];
+    <?php if ($newsletter_image && $newsletter_image_mobile) { ?>
+        
+        <?php
+            $mobile_aspectratio = [430, 430];
+        if ($newsletter_image_mobile) {
+            $mobile_aspectratio = [430, 430, $newsletter_image_mobile['ID']];
         }
         ?>
-        <?php display_responsive_image($newsletter_image['ID'],$cropOptions) ?>
-    <?php endif; ?>
+        <?php
+            $cropOptions = [
+                '(max-width: 768px)' => $mobile_aspectratio,
+                '(min-width: 769px)' => [952, 696],
+            ];
+        $attributes = ['picturetag_class' => 'h-100 w-50 absolute top-0 sm:w-100 sm:relative', 'class' => 'h-100 cover', 'loading' => 'lazy'];
+        ?>
+            <?php echo hatslogic_get_attachment_picture($newsletter_image['ID'], $cropOptions, $attributes); ?>
+    <?php } ?>
     <div class="flex align-center relative z-1">
         <div class="col w-50 sm:hidden"></div>
         <div class="col w-50 sm:w-100 p-80 sm:px-20 sm:py-60">
-            <?php if ($form_selector): ?>
+            <?php if ($form_selector) { ?>
                 <div class="form-wrap">
                     <div class="content">
-                        <?php echo do_shortcode('[contact-form-7 id="' . $form_selector->ID . '"]'); ?>
+                        <?php echo do_shortcode('[contact-form-7 id="'.$form_selector->ID.'"]'); ?>
                     </div>
                 </div>
-            <?php endif; ?>
+            <?php } ?>
         </div>
     </div>
 </section>
