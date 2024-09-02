4d688e4a62638187e10703e8dfb4ac10a7282e77
Vijith <vijith@2hatslogic.com>
2024-09-02 17:07:42 +0530

    header udpates for gtag

--- header.php
+++ header.php
@@ -34,57 +34,41 @@
 	<link rel="preload" href="<?php echo $get_dir; ?>/dist/assets/fonts/2HatsIcons/icomoon.woff2" as="font" type="font/woff2" crossorigin>
 	<link rel="preload" href="<?php echo $get_dir; ?>/dist/assets/img/brand/logo-wide.svg" as="image">
 	<style>@font-face {font-family: "TTFirsNeueTrl-Thin";src: url("<?php echo $get_dir; ?>/dist/assets/fonts/TTFirsNeueTrl/TTFirsNeueTrl-Thin.woff2") format("woff2"), url("<?php echo $get_dir; ?>/dist/assets/fonts/TTFirsNeueTrl/TTFirsNeueTrl-Thin.woff") format("woff");font-display: swap }@font-face {font-family: "TTFirsNeueTrl-Medium";src: url("<?php echo $get_dir; ?>/dist/assets/fonts/TTFirsNeueTrl/TTFirsNeueTrl-Medium.woff2") format("woff2"), url("<?php echo $get_dir; ?>/dist/assets/fonts/TTFirsNeueTrl/TTFirsNeueTrl-Medium.woff") format("woff");font-display: swap }@font-face {font-family: "BasisGrotesquePro-Light";src: url("<?php echo $get_dir; ?>/dist/assets/fonts/BasisGrotesquePro/BasisGrotesquePro-Light.woff2") format("woff2"), url("<?php echo $get_dir; ?>/dist/assets/fonts/BasisGrotesquePro/BasisGrotesquePro-Light.woff") format("woff");font-display: swap }@font-face {font-family: "BasisGrotesquePro-Regular";src: url("<?php echo $get_dir; ?>/dist/assets/fonts/BasisGrotesquePro/BasisGrotesquePro-Regular.woff2") format("woff2"), url("<?php echo $get_dir; ?>/dist/assets/fonts/BasisGrotesquePro/BasisGrotesquePro-Regular.woff") format("woff");font-display: swap }@font-face {font-family: "BasisGrotesquePro-Bold";src: url("<?php echo $get_dir; ?>/dist/assets/fonts/BasisGrotesquePro/BasisGrotesquePro-Bold.woff2") format("woff2"), url("<?php echo $get_dir; ?>/dist/assets/fonts/BasisGrotesquePro/BasisGrotesquePro-Bold.woff") format("woff");font-display: swap }@font-face {font-family: "PTSerif-Regular";src: url("<?php echo $get_dir; ?>/dist/assets/fonts/PTSerif/PTSerif-Regular.woff2") format("woff2"), url("<?php echo $get_dir; ?>/dist/assets/fonts/PTSerif/PTSerif-Regular.woff") format("woff");font-display: swap }@font-face {font-family: 'icomoon';src: url('<?php echo $get_dir; ?>/dist/assets/fonts/2HatsIcons/icomoon.woff2') format('woff2'), url('<?php echo $get_dir; ?>/dist/assets/fonts/2HatsIcons/icomoon.woff') format('woff');font-weight: normal;font-style: normal;font-display: block;}</style>
-	<link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.5.0/css/flag-icon.min.css" fetchpriority="low" as="style" onload="this.onload=null;this.rel='stylesheet'">
-
-	<?php // echo $gtm_head = get_field('gtm_head', 'options') ?? ''; ?>
 
 	<?php if(is_page_template('templates/template-shopware-migration.php')): ?>
-		<link rel="preload" fetchpriority="high" as="image" href="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/shopware-bg.jpeg" imagesrcset="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/shopware-bg.jpeg, <?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/shopware-bg2x.jpeg 2x" type="image/jpeg">
+		<link rel="preload" fetchpriority="high" as="image" href="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/shopware-bg.webp" imagesrcset="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/shopware-bg.webp, <?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/shopware-bg2x.webp 2x"  type="image/webp" media="(min-width: 768px)">
+		<link rel="preload" fetchpriority="high" as="image" href="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/shopware-bg-mobile.webp" imagesrcset="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/shopware-bg-mobile.webp 1x, <?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/shopware-bg-mobile.webp 2x"  type="image/webp" media="(max-width: 767px)">
 	<?php endif; ?>
 
 	<?php if(is_page_template('templates/template-hire-developer.php')): ?>
-		<link rel="preload" fetchpriority="high" as="image" href="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/laravel-bg.jpg" imagesrcset="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/laravel-bg.jpg, <?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/laravel-bg2x.jpg 2x" type="image/jpg">
+		<link rel="preload" fetchpriority="high" as="image" href="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/laravel-bg.webp" imagesrcset="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/laravel-bg.webp, <?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/laravel-bg2x.webp 2x"  type="image/webp" media="(min-width: 768px)">
+		<link rel="preload" fetchpriority="high" as="image" href="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/laravel-bg-mobile.webp" imagesrcset="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/laravel-bg-mobile.webp 1x, <?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/laravel-bg-mobile.webp 2x"  type="image/webp" media="(max-width: 767px)">
 	<?php endif; ?>
 
 	<?php if(is_page_template('templates/template-shopware-agency.php') || is_page_template('templates/template-agency.php')): ?>
-		<link rel="preload" fetchpriority="high" as="image" href="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shape-01.png" imagesrcset="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shape-01.png 1x, <?php echo get_template_directory_uri(); ?>/dist/assets/img/shape-012x.png 2x" type="image/png">
+		<link rel="preload" fetchpriority="high" as="image" href="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/shopware-bg.webp" imagesrcset="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/shopware-bg.webp, <?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/shopware-bg2x.webp 2x"  type="image/webp" media="(min-width: 768px)">
+		<link rel="preload" fetchpriority="high" as="image" href="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/shopware-bg-mobile.webp" imagesrcset="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/shopware-bg-mobile.webp 1x, <?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/shopware-bg-mobile.webp 2x"  type="image/webp" media="(max-width: 767px)">
 	<?php endif; ?>
 
 	<?php if(is_page_template('templates/template-services.php')): ?>
-		<link rel="preload" fetchpriority="high" as="image" href="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/service-bg.jpg" srcset="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/service-bg.jpg 1x, <?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/service-bg2x.jpg 2x" type="image/jpg">
+		<link rel="preload" fetchpriority="high" as="image" href="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/service-bg.webp" imagesrcset="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/service-bg.webp, <?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/service-bg2x.webp 2x"  type="image/webp" media="(min-width: 768px)">
+		<link rel="preload" fetchpriority="high" as="image" href="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/service-bg-mobile.webp" imagesrcset="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/service-bg-mobile.webp 1x, <?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/service-bg-mobile.webp 2x"  type="image/webp" media="(max-width: 767px)">
 	<?php endif; ?>
 
-	<?php if(is_post_type_archive('post')): ?>
-		<link rel="preload" fetchpriority="high" as="image" href="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/blog-bg.jpg" srcset="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/blog-bg.jpg 1x, <?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/blog-bg2x.jpg 2x" type="image/jpg">
+	<?php if(is_post_type_archive('post') || is_singular('post')): ?>
+		<link rel="preload" fetchpriority="high" as="image" href="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/blog-bg.webp" imagesrcset="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/blog-bg.webp, <?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/blog-bg2x.webp 2x"  type="image/webp" media="(min-width: 768px)">
+		<link rel="preload" fetchpriority="high" as="image" href="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/blog-bg-mobile.webp" imagesrcset="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/blog-bg-mobile.webp 1x, <?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/blog-bg-mobile.webp 2x"  type="image/webp" media="(max-width: 767px)">
 	<?php endif; ?>
 
+	<?php if(is_post_type_archive('case-study')): ?>
+		<link rel="preload" fetchpriority="high" as="image" href="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/case-study-bg.webp" imagesrcset="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/case-study-bg.webp, <?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/case-study-bg2x.webp 2x"  type="image/webp" media="(min-width: 768px)">
+		<link rel="preload" fetchpriority="high" as="image" href="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/case-study-bg-mobile.webp" imagesrcset="<?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/case-study-bg-mobile.webp 1x, <?php echo get_template_directory_uri(); ?>/dist/assets/img/shapes/case-study-bg-mobile.webp 2x"  type="image/webp" media="(max-width: 767px)">
+	<?php endif; ?>
 
 	<?php wp_head(); ?>
 
 	<script src="/~partytown/partytown.js"></script>
-	
-	<!-- Google Tag Manager -->
-	<script type="text/partytown">
-	(function(w,d,s,l,i){
-		w[l]=w[l]||[];
-		w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-		var f=d.getElementsByTagName(s)[0], j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
-		j.async=true;
-		j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-		f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-MMNRGHR');
-	</script>
-	<!-- End Google Tag Manager -->
-
-
-	<script type="text/partytown" src="https://www.googletagmanager.com/gtag/js?id=AW-793939830"></script>
-	<script type="text/partytown">
-	window.dataLayer = window.dataLayer || [];
-	function gtag(){dataLayer.push(arguments);}
-	gtag('js', new Date());
-	gtag('config', 'AW-793939830');
-	</script>
-
+	<?php echo $gtm_head = get_field('gtm_head', 'options') ?? ''; ?>
 	
 	<?php
 	if ( function_exists('get_field') ) {
@@ -107,12 +91,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-	<?php // echo $gtm_body= get_field('gtm_body', 'options') ?? ''; ?>
-
-	<!-- Google Tag Manager (noscript) -->
-	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MMNRGHR"
-	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-	<!-- End Google Tag Manager (noscript) -->
+	<?php echo $gtm_body= get_field('gtm_body', 'options') ?? ''; ?>
 	
 	<?php wp_body_open(); ?>
 	<script> var ajaxurl = '<?php echo admin_url('admin-ajax.php'); ?>'; </script>
@@ -124,9 +103,9 @@
 	<header class="header transition z-12 sticky top-0 w-100 md:bb-0 bg-white md:pb-20 md:pt-20">
 		<div class="container">
 			<div class="header-inner flex w-100 justify-between align-center">
-				<a href="<?php echo home_url(); ?>" class="brand" aria-label="<?php echo get_bloginfo('name'); ?>">
+				<a href="<?php echo home_url(); ?>" class="brand w-100 max-w-px-106 max-h-px-48" aria-label="<?php echo get_bloginfo('name'); ?>">
 					<img src="<?php echo $get_dir; ?>/dist/assets/img/brand/logo-wide.svg"
-						alt="<?php echo get_bloginfo('name'); ?>" class="w-100" width="106" height="48">
+						alt="<?php echo get_bloginfo('name'); ?>" class="w-100 w-100 max-w-px-106 max-h-px-48" width="106" height="48">
 				</a>
 
 				<?php 
@@ -136,7 +115,7 @@
 					'container' => 'nav',
 					'container_class' => 'top-menu transition block ml-auto mr-0 h-px-82 md:fixed md:z-12 md:h-100 md:w-100 md:left-0 md:top-0 md:pt-140 md:pb-100 md:pl-20 md:pr-20',
 					'menu_class' => 'main-menu no-bullets font-button flex md:column align-center fs-14 xxl:fs-16 lg:fs-15 md:fs-18',
-					'items_wrap' => '<a href="#" class="hidden md:visible brand absolute left-20 top-20" aria-label="2hatslogic"><img src="'.$get_dir.'/dist/assets/img/brand/logo-wide.svg" alt="'.get_bloginfo('name').'" class="w-100" width="106" height="48"> </a><button class="btn btn-secondary hidden md:visible absolute md:fixed z-3 top-20 right-20 close" onclick="closeMenu()"> <i class="icomoon icon-close"></i></button><ul id="%1$s" class="%2$s">%3$s</ul> <div class="flex bg-white hidden md:visible md:mt-30 md:fixed bottom-1 w-100 left-0 right-0 px-20">
+					'items_wrap' => '<a href="#" class="hidden md:visible brand w-100 max-w-px-106 max-h-px-48 absolute left-20 top-20" aria-label="2hatslogic"><img src="'.$get_dir.'/dist/assets/img/brand/logo-wide.svg" alt="'.get_bloginfo('name').'" class="w-100 w-100 max-w-px-106 max-h-px-48" width="106" height="48"> </a><button class="btn btn-secondary hidden md:visible absolute md:fixed z-3 top-20 right-20 close" onclick="closeMenu()"> <i class="icomoon icon-close"></i></button><ul id="%1$s" class="%2$s">%3$s</ul> <div class="flex bg-white hidden md:visible md:mt-30 md:fixed bottom-1 w-100 left-0 right-0 px-20">
 									<ul class="sub-menu no-bullets font-bold flex align-start b-0 bt-1 solid bc-hash w-100">
 										<li class="md:mt-20 md:mb-20">
 											<a href="'. home_url() .'/blog" class="inline-block" aria-label="blog">Blogs</a>
