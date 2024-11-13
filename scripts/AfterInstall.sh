46f8a9d8ff9c2d8e56c776e67e3596acab331464
radhu2hats <99962601+radhu2hats@users.noreply.github.com>
2024-11-13 18:06:29 +0530

    Update AfterInstall.sh
    Update AfterInstall.sh

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /var/www/html/2hatslogic-wp/wp-content/themes/2hatslogic2024
+cd /opt/bitnami/wordpress/wp-content/themes/2hatslogic2024
 /home/bitnami/.nvm/versions/node/v22.11.0/bin/yarn build
